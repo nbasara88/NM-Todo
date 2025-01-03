052d830c36e21b1e0c24a080e1f13e6669fd3c27
Coding <coding@Nesos-MacBook-Pro.local>
2025-01-02 21:34:51 -0500

    This is the initial Commit for the app

---
+++ NM Todo App/NM Todo App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NM Todo App
+//
+//  Created by Coding on 2025-01-02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NM Todo App/NM Todo App/NM_Todo_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NM_Todo_AppApp.swift
+//  NM Todo App
+//
+//  Created by Coding on 2025-01-02.
+//
+
+import SwiftUI
+
+@main
+struct NM_Todo_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
